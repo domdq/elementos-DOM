9b6da0b3b88c14c655690ec63d8d2f160a5860b1
Dom <domborgesbr2468@gmail.com>
2023-03-25 21:19:17 -0300

    feat: Response

---
+++ DOM-001/app.js
@@ -0,0 +1,5 @@
+const button = document.querySelector(".btn");
+const body = document.querySelector("body");
+button.addEventListener("click", function changeColor() {
+  body.style.backgroundColor = "#37306B";
+});
